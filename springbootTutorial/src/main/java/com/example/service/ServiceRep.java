b28a01a2e46b0aa0ab2e8243a8a1d493654f79f0
Vishnu Panchajanya Sudarsanam <78674110+srivishnu158@users.noreply.github.com>
2023-12-26 11:35:18 +0530

    Delete springbootTutorial directory

--- springbootTutorial/src/main/java/com/example/service/ServiceRep.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import org.springframework.stereotype.Service;
-
-import com.example.entity.Entities;
-@Service
-public interface ServiceRep {
-	public Entities saveDetails(Entities det);
-}
